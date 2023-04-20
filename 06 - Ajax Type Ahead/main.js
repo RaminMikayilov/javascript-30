fce2e920631710ce3b4e8afcde77aed232ee0ce3
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-04-20 18:29:22 +0400

    Add Ajax Type Ahead

---
+++ 06 - Ajax Type Ahead/main.js
@@ -0,0 +1,48 @@
+const input = document.querySelector(".input");
+const results = document.querySelector(".results");
+
+const api =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const cities = [];
+
+// fetch data
+fetch(api)
+  .then((data) => data.json())
+  .then((data) => cities.push(...data));
+
+const findCities = (word, cities) => {
+  return cities.filter((data) => {
+    const regex = new RegExp(word, "gi");
+    return data.city.match(regex) || data.state.match(regex);
+  });
+};
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+const displayCities = (e) => {
+  const word = e.target.value;
+  const citiesArray = findCities(word, cities);
+  const html = citiesArray
+    .map((place) => {
+      const regex = new RegExp(word, "gi");
+      const cityName = place.city.replace(
+        regex,
+        `<span class="hl">${word}</span>`
+      );
+      const stateName = place.state.replace(
+        regex,
+        `<span class="hl">${word}</span>`
+      );
+      return `
+        <div>
+          <span class="name">${cityName}, ${stateName}</span>
+          <span class="population">${numberWithCommas(place.population)}</span>
+        </div>
+      `;
+    })
+    .join("");
+  results.innerHTML = html;
+};
+
+input.addEventListener("input", displayCities);
