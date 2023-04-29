8e0f8467a248c91fde7227b84bb505bdbe3a330d
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-04-29 22:16:19 +0400

    Add Slide In on Scroll

---
+++ 13 - Slide In on Scroll/main.js
@@ -0,0 +1,36 @@
+const images = document.querySelectorAll(".slide-in");
+
+// function for decreasing the number of times the function is called
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkSlide() {
+  images.forEach((image) => {
+    const windowBottom = window.scrollY + window.innerHeight;
+    const slideInAt = windowBottom - image.height / 2;
+    const imageBottom = image.offsetTop + image.height;
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      image.classList.add("active");
+    } else {
+      image.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
