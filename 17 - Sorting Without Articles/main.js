68fb17206ce88aeff89a2e62ad4857ef899f119a
RaminMikayilov <raminmikayilov2003@gmail.com>
2023-05-02 01:01:30 +0400

    Add Sorting Without Articles

---
+++ 17 - Sorting Without Articles/main.js
@@ -0,0 +1,22 @@
+const bands = [
+  "The Plot in You",
+  "The Devil Wears Prada",
+  "Pierce the Veil",
+  "Norma Jean",
+  "The Bled",
+  "Say Anything",
+  "The Midway State",
+  "We Came as Romans",
+  "Counterparts",
+  "Oh, Sleeper",
+  "A Skylit Drive",
+  "Anywhere But Here",
+  "An Old Dog",
+];
+
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, "").trim();
+}
+
+const sortedArray = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+console.log(sortedArray);
